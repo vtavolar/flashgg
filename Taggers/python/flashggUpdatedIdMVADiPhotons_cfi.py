398908843e2d0dcf6506210f9422539cf6694032
vtavolar <vittorio.tavolaro@cern.ch>
2016-02-20 20:11:35 +0100

    read latest xml for ID mva EB with shifted r9, s4, EW

--- Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
+++ Taggers/python/flashggUpdatedIdMVADiPhotons_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 flashggUpdatedIdMVADiPhotons = cms.EDProducer("FlashggDiPhotonWithUpdatedPhoIdMVAProducer",
                                               src=cms.InputTag("flashggDiPhotons"),
                                               rhoFixedGridCollection = cms.InputTag('fixedGridRhoAll'),
-                                              photonIdMVAweightfile_EB = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_76X_25ns_barrel.xml"),
+                                              photonIdMVAweightfile_EB = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_76X_25ns_r9s4EtaWshift_barrel.xml"),
                                               photonIdMVAweightfile_EE = cms.FileInPath("flashgg/MicroAOD/data/MVAweights_76X_25ns_endcap.xml"),
                                               showerShapeTransfFile = cms.FileInPath("flashgg/Taggers/data/transformation_76X_v2.root"),
                                               correctShowerShapes = cms.bool(False),
