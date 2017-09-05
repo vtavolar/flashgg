796c4a5a819c9666993874b795ca7b130ccf571b
vtavolar <vittorio.tavolaro@cern.ch>
2017-09-05 13:51:40 +0200

    classes, tools and producer to have gen leptons with dressing in microAOD

--- MicroAOD/python/flashggMicroAODGenSequence_cff.py
+++ MicroAOD/python/flashggMicroAODGenSequence_cff.py
@@ -3,6 +3,9 @@ from flashgg.MicroAOD.flashggPrunedGenParticles_cfi import flashggPrunedGenParti
 from flashgg.MicroAOD.flashggGenPhotons_cfi import flashggGenPhotons
 from flashgg.MicroAOD.flashggGenPhotonsExtra_cfi import flashggGenPhotonsExtra
 
+from flashgg.MicroAOD.flashggGenLeptons_cfi import flashggGenLeptons
+from flashgg.MicroAOD.flashggGenLeptonsExtra_cfi import flashggGenLeptonsExtra
 
-flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra
+
+flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra + flashggGenLeptons*flashggGenLeptonsExtra
                                         )
