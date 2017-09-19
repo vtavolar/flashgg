6d74600518d2099049db0adad6e079752ac5de3c
vtavolar <vittorio.tavolaro@cern.ch>
2017-09-19 19:12:28 +0200

    remove unnecessary files

--- MicroAOD/python/flashggMicroAODGenSequence_cff.py
+++ MicroAOD/python/flashggMicroAODGenSequence_cff.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 from flashgg.MicroAOD.flashggPrunedGenParticles_cfi import flashggPrunedGenParticles
 from flashgg.MicroAOD.flashggGenPhotons_cfi import flashggGenPhotons
-from flashgg.MicroAOD.flashggGenBquarks_cfi import flashggGenBquarks
 from flashgg.MicroAOD.flashggGenPhotonsExtra_cfi import flashggGenPhotonsExtra
 
 from flashgg.MicroAOD.flashggGenLeptons_cfi import flashggGenLeptons
@@ -9,5 +8,5 @@ from flashgg.MicroAOD.flashggGenLeptonsExtra_cfi import flashggGenLeptonsExtra
 from flashgg.MicroAOD.flashggGenJetsExtra_cfi import flashggGenJetsExtra
 
 
-flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra + flashggGenLeptons*flashggGenLeptonsExtra + flashggGenJetsExtra + flashggGenBquarks
+flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra + flashggGenLeptons*flashggGenLeptonsExtra + flashggGenJetsExtra
                                         )
