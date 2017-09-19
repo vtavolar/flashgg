2d67290739fc5aadd7589a986bff005a8b3bc5b3
vtavolar <vittorio.tavolaro@cern.ch>
2017-09-19 19:11:53 +0200

    keep also b quarks in prunedGenParticles

--- MicroAOD/python/flashggMicroAODGenSequence_cff.py
+++ MicroAOD/python/flashggMicroAODGenSequence_cff.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 from flashgg.MicroAOD.flashggPrunedGenParticles_cfi import flashggPrunedGenParticles
 from flashgg.MicroAOD.flashggGenPhotons_cfi import flashggGenPhotons
+from flashgg.MicroAOD.flashggGenBquarks_cfi import flashggGenBquarks
 from flashgg.MicroAOD.flashggGenPhotonsExtra_cfi import flashggGenPhotonsExtra
 
 from flashgg.MicroAOD.flashggGenLeptons_cfi import flashggGenLeptons
@@ -8,5 +9,5 @@ from flashgg.MicroAOD.flashggGenLeptonsExtra_cfi import flashggGenLeptonsExtra
 from flashgg.MicroAOD.flashggGenJetsExtra_cfi import flashggGenJetsExtra
 
 
-flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra + flashggGenLeptons*flashggGenLeptonsExtra + flashggGenJetsExtra
+flashggMicroAODGenSequence = cms.Sequence(flashggPrunedGenParticles+flashggGenPhotons*flashggGenPhotonsExtra + flashggGenLeptons*flashggGenLeptonsExtra + flashggGenJetsExtra + flashggGenBquarks
                                         )
